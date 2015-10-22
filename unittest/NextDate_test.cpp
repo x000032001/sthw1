292e144f05f2af46bd3fbe9d3459ff80706233ac
x000032001 <s000032001@gmail.com>
2015-10-22 13:38:01 +0800

    add test case : 30days

--- unittest/NextDate_test.cpp
+++ unittest/NextDate_test.cpp
@@ -8,3 +8,10 @@ TEST( NextDate , MonthHas31Days )
 	ASSERT_EQ( "2000 08 01" , nextDate("2000 07 31") );
 	ASSERT_EQ( "2001 01 01" , nextDate("2000 12 31") );
 }
+
+TEST( NextDate , MonthHas30Days )
+{
+	ASSERT_EQ( "2000 04 02" , nextDate("2000 04 01") );
+	ASSERT_EQ( "2000 06 22" , nextDate("2000 06 23") );
+	ASSERT_EQ( "2000 12 01" , nextDate("2000 11 30") );
+}
