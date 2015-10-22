e519728e45127d87b5a1111bb94751bea3f66a9b
x000032001 <s000032001@gmail.com>
2015-10-22 13:33:59 +0800

    implement : 31days

--- src/NextDate.cpp
+++ src/NextDate.cpp
@@ -1,6 +1,43 @@
 #include "NextDate.h"
 
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
 string nextDate(string now)
 {
-	return "";
+	stringstream ss(now);
+	int year ,month ,day;
+	ss >> year >> month >> day;
+
+	if( month == 1 || month == 3
+	||  month == 5 || month == 7
+	||  month == 8 || month == 10
+	||  month == 12 )
+	{
+		if( day < 31 )
+		{
+			day++;
+		}
+		else if( day == 31 )
+		{
+			day = 1;
+			month++;
+			if( month == 13 )
+			{
+				month = 1;
+				year++;
+			}
+		}
+		else
+		{
+			return "error";
+		}
+	}
+
+	stringstream oss;
+	oss <<  year << " " 
+		<< setfill('0') << setw(2) << month << " "
+		<< setfill('0') << setw(2) << day;
+	return oss.str();
 }
--- unittest/NextDate_test.cpp
+++ unittest/NextDate_test.cpp
@@ -2,7 +2,9 @@
 
 TEST( NextDate , MonthHas31Days )
 {
-	ASSERT_EQ( "2000-01-20" , nextDate("2000-01-19") );
-	ASSERT_EQ( "2000-03-20" , nextDate("2000-03-19") );
-	ASSERT_EQ( "2000-09-20" , nextDate("2000-09-19") );
+	ASSERT_EQ( "2000 01 20" , nextDate("2000 01 19") );
+	ASSERT_EQ( "2000 03 20" , nextDate("2000 03 19") );
+	ASSERT_EQ( "2000 07 20" , nextDate("2000 07 19") );
+	ASSERT_EQ( "2000 08 01" , nextDate("2000 07 31") );
+	ASSERT_EQ( "2001 01 01" , nextDate("2000 12 31") );
 }
