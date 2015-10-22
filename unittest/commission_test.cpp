6bd73d59f2c0c68c048653bac5a319c3493f9a24
x000032001 <s000032001@gmail.com>
2015-10-22 16:50:33 +0800

    add test case : commission

--- unittest/commission_test.cpp
+++ unittest/commission_test.cpp
@@ -4,3 +4,15 @@ TEST( Commission , Zero )
 {
 	ASSERT_EQ( 0 , countCms(0,0,0) );
 }
+
+TEST( Commission , MoreThan1800 )
+{
+	ASSERT_EQ( 260 , countCms(0,0,80) );
+}
+
+TEST( Commission , MoreThan1000 )
+{
+	ASSERT_EQ( 103.75 , countCms(0,0,41) );
+	ASSERT_EQ( 175 , countCms(0,0,60) );
+}
+
